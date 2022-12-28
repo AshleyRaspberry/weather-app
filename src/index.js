0511fe91d2370cd93f3766631a8b741a8e678054
AshleyRaspberry <alafram1@gmail.com>
2022-12-28 10:52:37 -0600

    Removed  the Celsius temperature conversion.

--- src/index.js
+++ src/index.js
@@ -126,38 +126,9 @@ fahrenheitTemperature = response.data.main.temp;
   let currentLocationButton = document.querySelector("#current-location-btn");
   currentLocationButton.addEventListener("click", getCurrentLocation);
 
-  function displayCelsiusTemperature(event) {
-    event.preventDefault();
-    let temperature = document.querySelector("#temperature");
-    
-    fahrenheitLink.classList.remove("active");
-    celsiusLink.classList.add("active");
-
-    let celsiusTemperature = (fahrenheitTemperature - 32) * 5 / 9;
-    temperature.innerHTML = Math.round(celsiusTemperature);
-  }
-
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-
-  fahrenheitLink.classList.add("active");
-celsiusLink.classList.remove("active");
-  let temperature = document.querySelector("#temperature"); 
-  temperature.innerHTML = Math.round(fahrenheitTemperature); 
-  let fahrenheitTemperature = document.querySelector("#temperature").innerHTML;
-}
-
-let fahrenheitTemperature = null;
-
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-let celsiusLink = document.querySelector("#celsius-link"); 
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link"); 
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 searchCity("New Orleans");
